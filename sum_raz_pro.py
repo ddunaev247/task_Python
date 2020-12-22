ee5b246a3c4f8302cfc131ec08d59000cee08434
ddunaev247 <666satanik@mail.ru>
2020-12-22 17:18:47 +0300

    add rabota1

---
+++ sum_raz_pro.py
@@ -0,0 +1,12 @@
+#проект выводит сумму, разность и произведение 2х чисел которые задает пользователь.
+
+per1 = input("введите число а:", )
+per2 = input("введите число b", )
+per1 = float(per1)
+per2 = float(per2)
+summ = per1 + per2
+raz = per1 - per2
+pro = per1 * per2
+print("сумма:", summ)
+print("разность:", raz)
+print("произведение:", pro)
