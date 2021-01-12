ba362ba21432f2f95138ec8bd9080a2cd189563f
ddunaev247 <666satanik@mail.ru>
2021-01-12 13:25:07 +0300

    option for task_10

--- test4/task_10.py
+++ test4/task_10.py
@@ -4,8 +4,15 @@ list = [2, 3, 4, 5]
 dl_list = int(len(list))
 #print(dl_list)
 i = 0
-
 while i <= dl_list - 1:
     list[i] = list[i] * -2
     i += 1
 print(list)
+list1 = [2, 3, 5, 1]
+n = 0
+list2 = []
+for elem in list1:
+    list2.append(list1[n] * -2)
+    n += 1
+print(list2)
+
