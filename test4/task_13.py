97e36be8ff2a352e4536daa2439378c7e0da453d
ddunaev247 <666satanik@mail.ru>
2021-01-13 04:54:07 +0300

    option for in task_13

--- test4/task_13.py
+++ test4/task_13.py
@@ -1,9 +1,18 @@
 #Дан список. Создать новый список, сдвинутый на 1 элемент влево Пример:12345 -> 23451
 list = input("введите список:", )
 stp = 1
-
 list = list[stp:] + list[:stp]
-
 print(list)
+list1 = [1, 2, 3, 4]
+list2 = []
+len_list = len(list1)
+i = 0
+for id, elem in enumerate(list1):
 
+    if id + 1  == len_list:
+        list2.append(list1[0])
+    else:
+        list2.append(list1[i + 1])
+    i += 1    
+print(list2)
 
