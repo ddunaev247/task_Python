a66b6ab063701a4f220a78e59f48fa2014d635c2
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-02-24 15:42:11 +0300

    update task_05 for hw14

--- homework_7/task_05.py
+++ homework_7/task_05.py
@@ -1,7 +1,7 @@
 # Дан список строк. Отформатировать все строки в формате ‘{i} - {string}’, где i это порядковый номер
 # строки в списке. Использовать генератор списков.
 
-def format_list():
+def format_list() -> list:
     """This function formats a list of strings
 
     Input
@@ -10,17 +10,18 @@ def format_list():
 
     Result
     -------
-    The result will be a list of the form: {number} - {string} """
+    The result will be a list of the form: {number} - {string}
+    """
 
 
-    words = str(input('введите строки: '))
-    list = words.split()
-    list = ['{' + str(i) + '}' + ' - ' + '{' + word + '}' for i, word in enumerate(list, 1)]
-    print(list)
+    words = str(input('enter lines: '))
+    list_lines = words.split()
+    assert len(list_lines) != 0, 'the list must not be empty'
+    list_lines = ['{' + str(i) + '}' + ' - ' + '{' + word + '}' for i, word in enumerate(list_lines, 1)]
+    return list_lines
 
 if __name__ == "__main__":
-    format_list()
-
+    print(format_list())
 
 
 
